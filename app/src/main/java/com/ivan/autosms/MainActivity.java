ad09ed76cdbde8ec6e02605c64968c515486c1b1
Ivan Vezilic <eeone777@gmail.com>
2016-09-11 04:55:32 +0200

    added lots of stuff;

--- app/src/main/java/com/ivan/autosms/MainActivity.java
+++ app/src/main/java/com/ivan/autosms/MainActivity.java
@@ -2,64 +2,35 @@ package com.ivan.autosms;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Telephony;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
     private Sender s;
-    private EditText mEdit;
-    private EditText editPhone;
-
-    private void openDialog(Sender s) {
-        String messageText = String.format(
-                "This will send %d SMS for license plate %s. Your ticket will be valid until %s.",
-                s.getNumMessages(),
-                s.getLicensePlate(),
-                s.getEndTime());
-        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-        dialogBuilder.setMessage(messageText);
-        dialogBuilder.setCancelable(true);
-
-        final int n = s.getNumMessages();
-        final String ss = s.getLicensePlate();
-        dialogBuilder.setPositiveButton(
-            "OK",
-            new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                for (int i = 0; i < n; i++)
-                SmsManager.getDefault().sendTextMessage(editPhone.getText().toString(), null, ss, null, null);
-            }
-        });
-
-        dialogBuilder.setNegativeButton(
-            "Cancel",
-            new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                dialog.cancel();
-            }
-        });
-
-        AlertDialog sendDialog = dialogBuilder.create();
-        sendDialog.show();
-    }
+    private String sendDialogMessage = "This will send %d SMS for license plate %s. Your ticket will be valid until %s.";
+    private Date validUntilDate;
 
     private void askForPermissions() {
         // Here, thisActivity is the current activity
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.SEND_SMS)
-                != PackageManager.PERMISSION_GRANTED) {
-
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)!= PackageManager.PERMISSION_GRANTED) {
             // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.SEND_SMS)) {
@@ -81,16 +52,111 @@ public class MainActivity extends AppCompatActivity {
                 // result of the request.
             }
         }
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS)!= PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_SMS)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_SMS},
+                        0);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
     }
 
     public void onButtonClick(View view) {
-        Calendar c = Calendar.getInstance(Locale.getDefault());
-        int nMessages = 17 - c.get(Calendar.HOUR_OF_DAY);
+        s.sendClicked();
+    }
 
-        if (nMessages > 0) {
-            s = new Sender(nMessages, mEdit.getText().toString());
-            openDialog(s);
+    public void openSendDialog(int nMessages, String phone, String licensePlate, String endTimeText) {
+        String messageText = String.format(sendDialogMessage, nMessages, licensePlate, endTimeText);
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setMessage(messageText);
+        dialogBuilder.setCancelable(true);
+
+        dialogBuilder.setPositiveButton(
+            "OK",
+            new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    s.send();
+                }
+            }
+        );
+
+        dialogBuilder.setNegativeButton(
+            "Cancel",
+            new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+                }
+            }
+        );
+
+        AlertDialog sendDialog = dialogBuilder.create();
+        sendDialog.show();
+    }
+
+    public void openErrorDialog(String errorMessage) {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setMessage(errorMessage);
+        dialogBuilder.setCancelable(true);
+
+        dialogBuilder.setNegativeButton(
+                "Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                }
+        );
+
+        AlertDialog errorDialog = dialogBuilder.create();
+        errorDialog.show();
+    }
+
+    public void scanSMS(View view) {
+        String[] phoneNumber = new String[] { "9119" };
+
+        Cursor c = getApplicationContext().getContentResolver().query(
+                Uri.parse("content://sms/inbox"),
+                new String[] { "address", "date", "body" }, "address=?", phoneNumber, "date desc");
+
+        StringBuffer msgData = new StringBuffer();
+        if (c.moveToFirst()) {
+            String msgBody = c.getString(2);
+            String msgDate = msgBody.substring(22, 40);
+
+            Calendar cal = Calendar.getInstance(Locale.getDefault());
+            cal.set(Calendar.HOUR_OF_DAY, Integer.parseInt(msgDate.substring(0, 2)));
+            cal.set(Calendar.MINUTE, Integer.parseInt(msgDate.substring(3, 5)));
+            cal.set(Calendar.SECOND, 0);
+            cal.set(Calendar.DAY_OF_MONTH, Integer.parseInt(msgDate.substring(9, 11)));
+            cal.set(Calendar.MONTH, Integer.parseInt(msgDate.substring(12, 14)) - 1);
+            cal.set(Calendar.YEAR, 2000 + Integer.parseInt(msgDate.substring(15, 17)));
+
+            DateFormat df = new SimpleDateFormat("HH:mm dd.MM.yyyy");
+            Date validUntilDate = cal.getTime();
+
+            TextView validUntilText = (TextView)findViewById(R.id.validUntilDate);
+            validUntilText.setText(df.format(validUntilDate));
+        } else {
         }
+
+        c.close();
     }
 
     @Override
@@ -98,8 +164,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mEdit   = (EditText)findViewById(R.id.editText3);
-        editPhone = (EditText)findViewById(R.id.editText2);
+        s = new Sender(this);
+        s.numberPickerStart.setMinValue(0);
+        s.numberPickerStart.setMaxValue(24);
+        s.numberPickerStart.setValue(8);
+        s.numberPickerEnd.setMinValue(0);
+        s.numberPickerEnd.setMaxValue(24);
+        s.numberPickerEnd.setValue(17);
+
 
         askForPermissions();
     }
--- app/src/main/java/com/ivan/autosms/Sender.java
+++ app/src/main/java/com/ivan/autosms/Sender.java
@@ -1,5 +1,11 @@
 package com.ivan.autosms;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.telephony.SmsManager;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -9,35 +15,109 @@ import java.util.Locale;
  * Created by ivan on 8/20/16.
  */
 public class Sender {
-
-    private int nMessages = 0;
     private String licensePlate = "";
+    private String phone = "";
+    private String endTimeText = "";
+    private int nMessages;
 
-    public Sender () {
-        nMessages = 0;
-        licensePlate = "";
+    // App elements
+    private MainActivity mActivity;
+    private EditText editLPlate;
+    private EditText editPhone;
+    public NumberPicker numberPickerStart;
+    public NumberPicker numberPickerEnd;
+
+    private enum NMsgRetVal {
+        OK_TO_SEND,
+        SENT_ALREADY,
+        PAST_FIVE,
+        WEEKEND
     }
 
-    public Sender (int n, String lp) {
-        nMessages = n;
-        licensePlate = lp;
+    public Sender(MainActivity m) {
+        mActivity = m;
+        editLPlate  = (EditText)m.findViewById(R.id.editText3);
+        editPhone = (EditText)m.findViewById(R.id.editText2);
+        numberPickerStart = (NumberPicker)m.findViewById(R.id.numberPickerStart);
+        numberPickerEnd = (NumberPicker)m.findViewById(R.id.numberPickerEnd);
     }
 
-    public int getNumMessages() {
-        return nMessages;
-    }
+    private boolean checkInbox() { return false; }
 
-    public String getLicensePlate() {
-        return licensePlate;
-    }
-
-    public String getEndTime() {
+    public NMsgRetVal setNumMessages() {
         Calendar c = Calendar.getInstance(Locale.getDefault());
-        c.setTime(new Date());
-        c.add(Calendar.HOUR_OF_DAY, nMessages);
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int day = c.get(Calendar.DAY_OF_WEEK);
 
-        return new SimpleDateFormat("dd MMM yyyy HH:mm:ss").format(c.getTime());
+        int startOfDay = numberPickerStart.getValue();
+        int endOfDay = numberPickerEnd.getValue();
+
+        if (checkInbox()) {
+            return NMsgRetVal.SENT_ALREADY;
+        }
+
+        if (hour > endOfDay) {
+            return NMsgRetVal.PAST_FIVE;
+        }
+
+        if (day == c.SATURDAY || day == c.SUNDAY) {
+            return NMsgRetVal.WEEKEND;
+        }
+
+        c.setTime(new Date());
+        if (hour < startOfDay) {
+            nMessages = endOfDay - startOfDay;
+            c.add(Calendar.HOUR_OF_DAY, nMessages + startOfDay - hour);
+        }
+        else {
+            nMessages = endOfDay - hour;
+            c.add(Calendar.HOUR_OF_DAY, nMessages);
+        };
+        endTimeText = new SimpleDateFormat("dd MMM yyyy HH:mm:ss").format(c.getTime());
+
+        return NMsgRetVal.OK_TO_SEND;
     }
 
-    public void send() {    };
+    public void sendClicked() {
+        NMsgRetVal result = setNumMessages();
+
+        if (result == NMsgRetVal.OK_TO_SEND) {
+            openSendDialog();
+        }
+        else {
+            openErrorDialog(result);
+        }
+    }
+
+    private void openSendDialog() {
+        phone = editPhone.getText().toString();
+        licensePlate = editLPlate.getText().toString();
+
+        mActivity.openSendDialog(nMessages, phone, licensePlate, endTimeText);
+    }
+
+    private void openErrorDialog(NMsgRetVal r) {
+        String errorMessage = "";
+        switch(r) {
+            case PAST_FIVE:
+                errorMessage = "Time is past the zone end time.";
+                break;
+            case SENT_ALREADY:
+                errorMessage = "All messages are already sent.";
+                break;
+            case WEEKEND:
+                errorMessage = "Parking is free on weekends.";
+                break;
+            default:
+                break;
+        }
+
+        mActivity.openErrorDialog(errorMessage);
+    }
+
+    public void send() {
+        for (int i = 0; i < nMessages; i++) {
+            SmsManager.getDefault().sendTextMessage(phone, null, licensePlate, null, null);
+        }
+    };
 }
